840ac31c0182ff95d0059a604427e5dea40be4e3
Burcu Tanriverdi <58050979+burcutanriverdi@users.noreply.github.com>
2024-12-08 17:49:34 +0300

    add route, city and cities route attributes made.

---
+++ src/CountryMap.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class CountryMap {
+
+    City[] cities;
+    Route[] routes;
+    private int citySize;
+    private int routeSize;
+    public CountryMap(int citySize, int routeSize) {
+        this.cities = new City[citySize];
+        this.routes = new Route[routeSize];
+        this.citySize = citySize;
+        this.routeSize = routeSize;
+    }
+
+    private int cityCount= 0;
+    private int routeCount= 0;
+
+    public void addCity(String cityName){
+        if(cityCount < citySize){
+            cities[cityCount++] = new City(cityName);
+        }
+    }
+
+    public void addRoute(String from, String to, int dist){
+        if(cityFind(from) != null && cityFind(to) != null && routeCount < routeSize){
+            City fromCity = cityFind(from);
+            City toCity = cityFind(to);
+            routes[routeCount++] = new Route(fromCity, toCity, dist);
+        }
+    }
+
+    private City cityFind(String cityName){
+        for(City city : cities ){
+            if(city.getName().equals(cityName)){
+                return city;
+            }
+        }
+        return null;
+    }
+
+}
