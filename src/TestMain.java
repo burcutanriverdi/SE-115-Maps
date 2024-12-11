c5052f8d3a83bc52d30b3669f1c4aaf131cb0c33
Burcu Tanrıverdi <58050979+burcutanriverdi@users.noreply.github.com>
2024-12-12 02:16:06 +0300

    new file tested with automated start and end point reader

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class TestMain {
     public static void main(String[] args) {
 
-        String fileName = "map1.txt";
+        String fileName = "map2.txt";
         FileRead fileRead = new FileRead();
         CountryMap countryMap = fileRead.readCountryMap(fileName);
 
         if (countryMap != null) {
             WayFinder wf = new WayFinder(countryMap);
-            wf.findFastestRoute("A", "E");
+            wf.findFastestRoute(countryMap.getStartCity(), countryMap.getEndCity());
 
             FileWrite fileWrite = new FileWrite();
             //String output = wf.findFastestRoute("A", "E").printRoute();
-            fileWrite.writeToFile("output.txt", wf.writePath("A", "E"));
+            fileWrite.writeToFile("output.txt", wf.writePath(countryMap.getStartCity(), countryMap.getEndCity()));
         }
     }
 
