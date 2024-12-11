28042e41315eb15792bfd075e2d199caca3a324d
Burcu Tanrıverdi <58050979+burcutanriverdi@users.noreply.github.com>
2024-12-12 00:48:55 +0300

    WayFinder class added
    Implementing Dijkstra algorithm to the class and finding the shortest path

---
+++ src/WayFinder.java
@@ -0,0 +1,106 @@
+public class WayFinder {
+
+    private CountryMap countryMap;
+    private int[] distances;
+    private boolean[] visited;
+    private int[] previous;
+
+    public WayFinder(CountryMap countryMap) {
+        this.countryMap = countryMap;
+    }
+
+    // Method to find the fastest route iwmplementing Dijkstra algorithm
+    public void findFastestRoute(String startName, String endName) {
+
+        int startCity = countryMap.cityIndexFind(startName);
+        int endCity = countryMap.cityIndexFind(endName);
+        if (startCity == -1 || endCity == -1) {
+            System.out.println("City that you want to go is not in the map");
+            return;
+        }
+
+        int size = countryMap.cities.length;
+
+        distances = new int[size];
+        visited = new boolean[size];
+        previous = new int[size];
+
+        int min = Integer.MAX_VALUE;
+        int minIndex = -1;
+        for (int i = 0; i < size; i++) {
+            distances[i] = Integer.MAX_VALUE;
+            visited[i] = false;
+            previous[i] = -1;
+        }
+        distances[startCity] = 0;
+        for (int i = 0; i < size; i++) {
+            int current = getMinDistanceUnvisitedIndex();
+
+            if (current == -1) {
+                break;
+            }
+
+            visited[current] = true;
+
+            for (Route route : countryMap.routes) {
+                if (route == null) continue;
+
+                int fromIndex = countryMap.cityIndexFind(route.getCity1().getName());
+                int toIndex = countryMap.cityIndexFind(route.getCity2().getName());
+
+
+                if (fromIndex == current) {
+                    int newDist = distances[current] + route.getDistance();
+                    if (newDist < distances[toIndex]) {
+                        distances[toIndex] = newDist;
+                        previous[toIndex] = current;
+                    }
+                }
+
+                if (toIndex == current) {
+                    int newDist = distances[current] + route.getDistance();
+                    if (newDist < distances[fromIndex]) {
+                        distances[fromIndex] = newDist;
+                        previous[fromIndex] = current;
+                    }
+                }
+            }
+        }
+
+        if (distances[endCity] == Integer.MAX_VALUE) {
+            System.out.println("No path found from " + startName + " to " + endName);
+        } else {
+            // Reconstruct path
+            printPath(startCity, endCity);
+            System.out.println("Total Time: " + distances[endCity] + " min");
+        }
+    }
+
+
+    private int getMinDistanceUnvisitedIndex() {
+        int minDist = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        for (int i = 0; i < distances.length; i++) {
+            if (!visited[i] && distances[i] < minDist) {
+                minDist = distances[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public void printPath(int start, int end) {
+        if (start == end) {
+            System.out.print(countryMap.cities[start].getName());
+        } else if (previous[end] == -1) {
+            System.out.println("No path found");
+        } else {
+            printPath(start, previous[end]);
+            System.out.print( " -> " + countryMap.cities[end].getName() + " ");
+        }
+
+    }
+}
+
+
