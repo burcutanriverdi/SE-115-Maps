229cda83021875969434774f6d5f99f20d8b90ee
Burcu Tanrıverdi <58050979+burcutanriverdi@users.noreply.github.com>
2024-12-12 18:11:16 +0300

    Taking the file name from the command line

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,9 +1,21 @@
-import java.util.Scanner;
+import java.io.File;
 
 public class TestMain {
     public static void main(String[] args) {
+        // Check if a file name is provided as a command-line argument
+        if (args.length < 1) {
+            System.out.println("Usage: java TestMain <file_name>");
+            return;
+        }
 
-        String fileName = "map2.txt";
+        String fileName = args[0]; // Get the file name from the command line
+        File file = new File(fileName);
+
+        if (!file.exists()) {
+            System.out.println("Error: File not found - " + fileName);
+            return;
+        }
+
         FileRead fileRead = new FileRead();
         CountryMap countryMap = fileRead.readCountryMap(fileName);
 
@@ -12,8 +24,9 @@ public class TestMain {
             wf.findFastestRoute(countryMap.getStartCity(), countryMap.getEndCity());
 
             FileWrite fileWrite = new FileWrite();
-            //String output = wf.findFastestRoute("A", "E").printRoute();
             fileWrite.writeToFile("output.txt", wf.writePath(countryMap.getStartCity(), countryMap.getEndCity()));
+        } else {
+            System.out.println("Error: Unable to read the country map from the file.");
         }
     }
 }
