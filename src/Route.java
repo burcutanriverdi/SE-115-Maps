49ed325b57e1406fbc147c210aa652d5e60844ee
Burcu Tanrıverdi <58050979+burcutanriverdi@users.noreply.github.com>
2024-12-20 23:10:13 +0300

    removed unused setter methods

--- src/Route.java
+++ src/Route.java
@@ -13,23 +13,11 @@ public class Route {
         return city1;
     }
 
-    public void setCity1(City city1) {
-        this.city1 = city1;
-    }
-
     public City getCity2() {
         return city2;
     }
 
-    public void setCity2(City city2) {
-        this.city2 = city2;
-    }
-
     public int getDistance() {
         return distance;
-    }
-
-    public void setDistance(int distance) {
-        this.distance = distance;
     }
 }
